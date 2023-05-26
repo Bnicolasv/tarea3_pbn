8ee187cc462aa104e1ef23102a467898120b30ab
Benjamin  Valenzuela Prieto <benjaminvalenzuelaprieto@MacBook-Pro-de-Benjamin.local>
2023-05-26 11:59:50 -0400

    Intento de lectura

--- main.cpp
+++ main.cpp
@@ -1,49 +1,64 @@
 #include <iostream>
 #include <string>
 #include <cstring>
-// #include "posicion.h"
-#include "mapa.h"
+#include <sstream>
+#include <fstream>
+#include "personaje.h"
+#include <vector>
 
-using namespace std;
 
 int main () {
 
-    // Probandando Getter y Setter de Posicion
-    // Posicion p(3, 4);
-    // cout << p.getX() << endl; // Imprime 3
-    // cout << p.getY() << endl; // Imprime 4
+    std::ifstream archivo("archivo.txt"); // Nombre del archivo a leer
 
-    // p.set(10, 12);
-    // cout << p.getX() << endl; // Ahora imprime 10
-    // cout << p.getY() << endl; // Ahora imprime 12
-
-
-    // CREAR TABLERO
-    int ANCHO = 10;
-    int ALTO = 10;
-
-    char arr[3][3][7] = {{"Jose", "Pedro", "Juan"}, {"Matias", "Jose", "Juan"}, {"Pedro", "Pablo", "Benja"}};
-    char arr2[3][3][2] = {{"1", "2", "1"}, {"1", "1", "1"}, {"2", "2", "1"}};
-
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            if (strlen(arr[i][j]) == 5) {
-                cout << "|" << arr[i][j];
-            } else {
-                cout << "|" << arr[i][j];
-                for (int k = 0; k < 9 - strlen(arr[i][j]); k++) {
-                    cout << " ";
-                }
-            }
-        }
-        cout << "|" << endl;
-        cout << "|          |          |" << endl;
+    if (!archivo) {
+        std::cerr << "Error al abrir el archivo." << std::endl;
+        return 1;
     }
 
+    std::string linea;
+    std::getline(archivo, linea);
 
-    // Probando obtencion de casilla central
-    Mapa mapa(9, 7);
-    cout << "(" << mapa.casillaCentralX() << "," << mapa.casillaCentralY() << ")" << endl;
+    std::stringstream dimensiones(linea);
+    int ancho, alto;
+    dimensiones >> ancho >> alto;
+
+    std::getline(archivo, linea);
+    std::stringstream soldados1(linea);
+    int cantidad_de_soldados1;
+    soldados1 >> cantidad_de_soldados1;
+
+    std::vector<Personaje> soldados_equipo1;
+
+    for (int i = 0; i < cantidad_de_soldados1; ++i) {
+        std::getline(archivo, linea);
+        std::stringstream soldado1(linea);
+        std::string nombre;
+        int vida, ataque, velocidad, pos_x, pos_y;
+        soldado1 >> nombre >> vida >> ataque >> velocidad >> pos_x >> pos_y;
+        Posicion p = Posicion(pos_x, pos_y);
+        soldados_equipo1.emplace_back(nombre, vida, ataque, velocidad, p);
+    }
+
+    std::getline(archivo, linea);
+    std::stringstream soldados2(linea);
+    int cantidad_de_soldados2;
+    soldados2 >> cantidad_de_soldados2;
+
+    std::vector<Personaje> soldados_equipo2;
+
+    for (int i = 0; i < cantidad_de_soldados2; ++i) {
+        std::getline(archivo, linea);
+        std::stringstream soldado2(linea);
+        std::string nombre;
+        int vida, ataque, velocidad, pos_x, pos_y;
+        soldado2 >> nombre >> vida >> ataque >> velocidad >> pos_x >> pos_y;
+        Posicion p = Posicion(pos_x, pos_y);
+        soldados_equipo2.emplace_back(nombre, vida, ataque, velocidad, p);
+    }
+
+    archivo.close();
+
     return 0;
 }
 
